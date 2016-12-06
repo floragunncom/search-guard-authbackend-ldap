068250c781b210c686adc752235ae601e06388a0
floragunn <dev@search-guard.com>
2016-12-06 22:13:31 +0100

    add testcase for authorization only case

--- src/test/java/com/floragunn/dlic/auth/ldap/LdapBackendTest.java
+++ src/test/java/com/floragunn/dlic/auth/ldap/LdapBackendTest.java
@@ -430,6 +430,30 @@ public class LdapBackendTest {
         Assert.assertEquals(2, user.getRoleEntries().size());
         Assert.assertEquals(user.getName(), user.getUserEntry().getDn());
     }
+    
+    @Test
+    public void testLdapAuthorizationOnly() throws Exception {
+
+        startLDAPServer();
+
+        final Settings settings = Settings.builder()
+                .putArray(ConfigConstants.LDAP_HOSTS, "localhost:" + EmbeddedLDAPServer.ldapPort)
+                .put(ConfigConstants.LDAP_AUTHC_USERSEARCH, "(uid={0})")
+                .put(ConfigConstants.LDAP_AUTHC_USERBASE, "ou=people,o=TEST")
+                .put(ConfigConstants.LDAP_AUTHZ_ROLEBASE, "ou=groups,o=TEST")
+                .put(ConfigConstants.LDAP_AUTHZ_ROLENAME, "cn")
+                .put(ConfigConstants.LDAP_AUTHZ_ROLESEARCH, "(uniqueMember={0})")
+                .build();
+
+        final User user = new User("jacksonm");
+
+        new LDAPAuthorizationBackend(settings).fillRoles(user, null);
+
+        Assert.assertNotNull(user);
+        Assert.assertEquals("jacksonm", user.getName());
+        Assert.assertEquals(2, user.getRoles().size());
+        Assert.assertEquals("ceo", new ArrayList(new TreeSet(user.getRoles())).get(0));
+    }
 
     @After
     public void tearDown() throws Exception {
